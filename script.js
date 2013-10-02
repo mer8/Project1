c31a9eb702730286edeb9db6d779b0985cd24614
Meredith Bryan <merbryan@gmail.com>
2013-10-02 15:38:15 -0700

    Diagonal win implemented & new pics

--- script.js
+++ script.js
@@ -42,43 +42,44 @@ var turn =1;
 function playBox()
 {
 
+//*****Switches whether its an X or and O*****
 if(event.target.innerHTML == "")  {
 
 		switch(turn) {
 		case 1:
-		event.target.innerHTML = "X";
+		event.target.innerHTML = "<img src='cat2pic.gif' />";
 		event.target.value = 1;
 		break;
 		case 2:
-		event.target.innerHTML = "<img src='100x100cat.png' />";
+		event.target.innerHTML = "<img src='typingcat.gif' />";
 		event.target.value = -1;
 		break;
 		case 3:
-		event.target.innerHTML = "X";
+		event.target.innerHTML = "<img src='cat2pic.gif' />";
 		event.target.value = 1;
 		break;
 		case 4:
-		event.target.innerHTML = "<img src='100x100cat.png' />";
+		event.target.innerHTML = "<img src='typingcat.gif' />";
 		event.target.value = -1;
 		break;
 		case 5:
-		event.target.innerHTML = "X";
+		event.target.innerHTML = "<img src='cat2pic.gif' />";
 		event.target.value = 1;
 		break;
 		case 6:
-		event.target.innerHTML = "<img src='100x100cat.png' />";
+		event.target.innerHTML = "<img src='typingcat.gif' />";
 		event.target.value = -1;
 		break;
 		case 7:
-		event.target.innerHTML = "X";
+		event.target.innerHTML = "<img src='cat2pic.gif' />";
 		event.target.value = 1;
 		break;
 		case 8:
-		event.target.innerHTML = "<img src='100x100cat.png' />";
+		event.target.innerHTML = "<img src='typingcat.gif' />";
 		event.target.value = -1;
 		break;
 		case 9:
-		event.target.innerHTML = "X";
+		event.target.innerHTML = "<img src='cat2pic.gif' />";
 		event.target.value = 1;
 		break;
 		} 
@@ -116,21 +117,47 @@ for(r=0;r<=2;++r)
 
 
 {
-
+// **This is for row and column wins**
 
  	if (cellArray[0][c] == cellArray[1][c] && 
  		cellArray[1][c] == cellArray[2][c] && 
  		cellArray[0][c] != "")
- 		alert(cellArray[0][c] +"won in column" + c);
+ 		alert(cellArray[0][c] +" won in column" + c);
 
 
  	if (cellArray[c][0] == cellArray[c][1] && 
  		cellArray[c][1] == cellArray[c][2] && 
  		cellArray[c][0] != "")
- 		alert(cellArray[c][0] +"won in column" + c);
+ 		alert(cellArray[c][0] +" won in row" + c);
 
 
  }
 
+ // **This is for diagonal wins**
+
+  	if (cellArray[0][0] == cellArray[1][1] &&
+ 		cellArray[1][1] == cellArray[2][2] &&
+ 		cellArray[2][2] != "")
+ 		alert(cellArray[2][2] +" won in diag" + c);
+
+ 	if (cellArray[0][2] == cellArray[1][1] &&
+ 		cellArray[1][1] == cellArray[2][0] &&
+ 		cellArray[2][0] != "")
+ 		alert(cellArray[2][0] +" won in diag" + c);
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
